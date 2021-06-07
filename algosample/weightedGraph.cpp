4120d6fb89938967cc1bf87ee57fe23fe6d12a14
onikun94 <onikun94@gmail.com>
2021-06-07 23:19:07 +0900

    weightedGraph

---
+++ algosample/weightedGraph.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Edge
+{
+    int to;
+    long long w;
+    Edge(int to, long long w) : to(to), w(w) {}
+};
+
+using Graph = vector<vector<Edge>>;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+
+    Graph G(N);
+    for (int i = 0; i < M; i++)
+    {
+        int a, b;
+        long long w;
+        cin >> a >> b >> w;
+        G[a].push_back(Edge(b, w));
+    }
+}
